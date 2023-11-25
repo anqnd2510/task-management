cf258905b832c18490a934cc76713b9ee3575ea2
anqnd2510 <duyan251003@gmail.com>
2023-11-25 16:03:50 +0700

    Pagination

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -1,23 +1,42 @@
 const Task = require("../models/task.model");
+
+const paginationHelper = require("../../v1/helpers/pagination");
+
 // [GET] /api/v1/tasks
 module.exports.index = async (req, res) => {
   const find = {
-    deleted: false
+    deleted: false,
   };
-  if(req.query.status) {
+
+  if (req.query.status) {
     find.status = req.query.status;
   }
 
-  console.log(req.query)
+  // Pagination
+  let initPagination = {
+    currentPage: 1,
+    limitItems: 2,
+  };
+  const countTasks = await Task.countDocuments(find);
+  const objectPagination = paginationHelper(
+    initPagination,
+    req.query,
+    countTasks
+  );
+  // End Pagination
+
   // Sort
   const sort = {};
 
-  if(req.query.sortKey && req.query.sortValue) {
+  if (req.query.sortKey && req.query.sortValue) {
     sort[req.query.sortKey] = req.query.sortValue;
   }
   // End Sort
 
-  const tasks = await Task.find(find).sort(sort);
+  const tasks = await Task.find(find)
+    .sort(sort)
+    .limit(objectPagination.limitItems)
+    .skip(objectPagination.skip);
 
   res.json(tasks);
 };
@@ -25,11 +44,12 @@ module.exports.index = async (req, res) => {
 module.exports.detail = async (req, res) => {
   try {
     const id = req.params.id;
+
     const task = await Task.findOne({
       _id: id,
-      deleted: false
+      deleted: false,
     });
-  
+
     res.json(task);
   } catch (error) {
     res.json("Không tìm thấy!");
