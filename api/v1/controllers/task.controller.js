eba1a2a810cb30fd723b88e375876044c934fbd5
anqnd2510 <duyan251003@gmail.com>
2023-11-25 16:12:37 +0700

    Search

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -2,15 +2,23 @@ const Task = require("../models/task.model");
 
 const paginationHelper = require("../../v1/helpers/pagination");
 
+const searchHelper = require("../../v1/helpers/search");
+
 // [GET] /api/v1/tasks
 module.exports.index = async (req, res) => {
   const find = {
     deleted: false,
   };
-
   if (req.query.status) {
     find.status = req.query.status;
   }
+  //  Search
+  let objectSearch = searchHelper(req.query);
+
+  if (req.query.keyword) {
+    find.title = objectSearch.regex;
+  }
+  //  End Search 
 
   // Pagination
   let initPagination = {
@@ -24,32 +32,26 @@ module.exports.index = async (req, res) => {
     countTasks
   );
   // End Pagination
-
   // Sort
   const sort = {};
-
   if (req.query.sortKey && req.query.sortValue) {
     sort[req.query.sortKey] = req.query.sortValue;
   }
   // End Sort
-
   const tasks = await Task.find(find)
     .sort(sort)
     .limit(objectPagination.limitItems)
     .skip(objectPagination.skip);
-
   res.json(tasks);
 };
 // [GET] /api/v1/tasks/detail/:id
 module.exports.detail = async (req, res) => {
   try {
     const id = req.params.id;
-
     const task = await Task.findOne({
       _id: id,
       deleted: false,
     });
-
     res.json(task);
   } catch (error) {
     res.json("Không tìm thấy!");
