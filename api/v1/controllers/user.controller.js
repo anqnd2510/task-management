f1c1fba36a4983d5d641433983fc4549a56801a9
anqnd2510 <duyan251003@gmail.com>
2023-11-28 00:35:14 +0700

    detail user

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -187,3 +187,17 @@ module.exports.forgotPassword = async (req, res) => {
       message: "Đổi mật khẩu thành công!"
     });
   }
+
+  // [GET] /api/v1/users/detail
+  module.exports.detail = async (req, res) => {
+    const token = req.cookies.token;
+    const user = await User.findOne({
+      token: token,
+      deleted: false
+    }).select("-password -token");
+    res.json({
+      code: 200,
+      message: "Thành công!",
+      info: user
+    });
+  };
