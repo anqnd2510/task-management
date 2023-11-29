7bca52b04584dc726d11ffa5be8cba251e645dc7
anqnd2510 <duyan251003@gmail.com>
2023-11-29 15:32:13 +0700

    Task công việc nhỏ khác

--- api/v1/models/task.model.js
+++ api/v1/models/task.model.js
@@ -8,6 +8,7 @@ const taskSchema = new mongoose.Schema(
         timeStart: Date,
         timeFinish: Date,
         createdBy: String,
+        taskParentId: String,
         deleted: {
             type: Boolean,
             default: false,
